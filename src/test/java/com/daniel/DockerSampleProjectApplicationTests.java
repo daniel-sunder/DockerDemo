0cf9beba589882c5381b240369af0a789865278f
Daniel Sunderland <daniel.sunde@hcl.com>
2021-06-17 11:54:33 -0500

    first commit

---
+++ src/test/java/com/daniel/DockerSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
